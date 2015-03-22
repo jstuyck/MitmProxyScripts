67c98ba67f99d5de5022b32fdb3eb9cd0d96908f
Julien Stuyck <julien.stuyck@techdrill.com>
2015-03-22 23:28:01 +0000

    Add missing line on the script and some comment

--- scripts/tappedout.py
+++ scripts/tappedout.py
@@ -1,10 +1,13 @@
 from binascii import unhexlify
 
+#This can be replace by using the "decode" function on the reponse
 def request(context, flow):
     if (flow.request.host.find('change.me') > -1 and flow.request.path.find('somethingaboutcurrency') > -1):
-       
+       flow.request.headers['Accept-Encoding'] = ['']
+
 
 def response(context, flow):
     
     if (flow.request.host.find('change.me') > -1 and flow.request.path.find('somethingaboutcurrency') > -1):
-        flow.response.content = unhexlify('[..]ffffff[..]')
+        flow.response.content = unhexlify('[..]ffffff[..]') 
+        #this ca be enhanced by using the protobuf to deserialize the message
